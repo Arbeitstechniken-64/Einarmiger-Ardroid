e272f662528fa3e9d77e2415c50a74c5434609ae
Simon Struck <simon@simonscode.org>
2018-11-05 17:23:24 +0100

    Mayor work
    Add number to 7 segment translations
    Add central state machine
    Add interrupts
    Add basic I/O

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,7 +1,39 @@
 #include <Arduino.h>
 
+///////////////////////////////////////
+////             Pins              ////
+///////////////////////////////////////
+
+// Serial data out to shift registers
+const int dataPin = 8;
+// Latch to update the current value of the registers
+const int latchPin = 11;
+// Clock for serial data to shift registers
+const int clockPin = 12;
+
+// Interrupt for buttons
+const int interruptPin = 2;
+// Input to start game
+const int triggerPin = 5;
+// Input to add 50 cents to the balance
+const int fivetyCentPin = 6;
+// Input to add one euro to the balance
+const int oneEuroPin = 7;
+// Input to add two euros to the balance
+const int twoEurosPin = 8;
+
+///////////////////////////////////////
+////          Constants            ////
+///////////////////////////////////////
+
+// Time between frames 1000/50 = 20 fps
 const int frameTime = 50;
-const byte animations[1] = {0b11111111};
+// Time after a spin to wait before resuming idle animation
+const int waitBeforeIdle = 10000;
+// Time to display the spinup animation
+const int spinupTime = 1500;
+// Time to display the spindown animation
+const int spindownTime = 1000;
 
 /*
 Bit order from left to right
@@ -18,7 +50,7 @@ Bit order from left to right
    --7--
 
 */
-
+// Characters in 7-segment form
 const byte characters[10] = {
     0b11100111, // 0
     0b00100010, // 1
@@ -32,30 +64,56 @@ const byte characters[10] = {
     0b11111011, // 9
 };
 
-float balance = 0.0;
-int currentAnimationFrame = 0;
-int currentAnimation = 0;
-int framesSinceLastPlay = 0;
-uint16_t test = 0;
+// Characters to display the word HELLO in 7-segment form
+const byte welcome[5] = {
+    0b01111110, // H
+    0b10111011, // E
+    0b01000101, // L
+    0b01000101, // L
+    0b11100111, // O
+};
 
-void setup() {
-  // setup
-}
+///////////////////////////////////////
+////         State machine         ////
+///////////////////////////////////////
+
+enum State { IDLE, SPINUP, SPINNING, SPINDOWN, WAITING };
+
+// the current state of the machine
+// this has to be volatile according to the documentation for interrupts
+volatile State currentState = IDLE;
+
+///////////////////////////////////////
+////          Variables            ////
+///////////////////////////////////////
+
+// Amount of money belonging to the human
+// this has to be volatile according to the documentation for interrupts
+volatile float balance = 0.0;
+//
+unsigned long spinStartTime = 0;
+unsigned long spinEndTime = 0;
+
+int resultA = 0;
+int resultB = 0;
+int resultC = 0;
+
+int speedA = 0;
+int speedB = 0;
+int speedC = 0;
+
+///////////////////////////////////////
+////       Helper functions        ////
+///////////////////////////////////////
+
+void handleInterrupt() {}
 
 void showOutcome(int a, int b, int c) {
   // stop the 'rolls' and show outcome
 }
 
-bool isLeverPulled() {
-  return false;
-  // add proper check later (or handle with interrupt)
-}
-
-bool hasAdded50Cent() { return false; }
-bool hasAdded1Euro() { return false; }
-bool hasAdded2Euros() { return false; }
-
 void nextAnimationFrame() {
+
   // render next animation frame
 }
 
@@ -63,46 +121,55 @@ void renderBalance() {
   // show balance on display
 }
 
-void addWinningsToBalance() { balance += 0.5; }
-
-void incrementFunds(float addedBalance) {
-  balance += addedBalance;
-  renderBalance();
+void spin() {
+  currentState = SPINNING;
+  resultA = random(0, 9);
+  resultB = random(0, 9);
+  resultC = random(0, 9);
+  spinStartTime = millis();
+  spinEndTime = millis() + spinupTime + (unsigned long)(random(5, 10) * 1000) +
+                spindownTime;
 }
 
-void spin() {
-  int resultA = random(0, 9);
-  int resultB = random(0, 9);
-  int resultC = random(0, 9);
-  int spinningTime = random(5, 10);
-  int currentTime = 0;
+///////////////////////////////////////
+////          Main loop            ////
+///////////////////////////////////////
 
-  while (currentTime < spinningTime) {
-    nextAnimationFrame();
-    delay(frameTime);
-    spinningTime += frameTime;
+void setup() {
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(latchPin, OUTPUT);
+  pinMode(interruptPin, INPUT);
+  pinMode(triggerPin, INPUT);
+  pinMode(fivetyCentPin, INPUT);
+  pinMode(oneEuroPin, INPUT);
+  pinMode(twoEurosPin, INPUT);
+
+  for (unsigned int i = 0; i < sizeof(welcome); i++) {
+    shiftOut(dataPin, clockPin, MSBFIRST, welcome[i]);
   }
-  showOutcome(resultA, resultB, resultC);
-  int someTime = 7;
-  delay(someTime);
-  if (winner) {
-    addWinningsToBalance();
-  }
+  attachInterrupt(digitalPinToInterrupt(interruptPin), handleInterrupt, RISING);
 }
 
 void loop() {
-  if (isLeverPulled()) {
-    spin();
-  } else if (hasAdded50Cent()) {
-    incrementFunds(0.5);
-  } else if (hasAdded1Euro()) {
-    incrementFunds(1.0);
-  } else if (hasAdded2Euros()) {
-    incrementFunds(2.0);
-  } else if (framesSinceLastPlay > 2048 &&
-             balance <= 0.0) { // wait a bit after the last game
-    nextAnimationFrame();
-  } else {
-    delay(frameTime);
+  switch (currentState) {
+  case IDLE:
+    // play idle animation
+    break;
+  case SPINUP:
+    // play spinup animation
+    break;
+  case SPINNING:
+    // play spinning animation
+    break;
+  case SPINDOWN:
+    // play spindown animation
+    break;
+  case WAITING:
+    // do not play an animation
+    if (millis() > spinEndTime + waitBeforeIdle) {
+      currentState = IDLE;
+    }
+    break;
   }
 }
